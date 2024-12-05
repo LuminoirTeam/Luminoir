b0560e09e7a6727066e68ce76ae2a49331aa5ca7
NATEEEUUUH <nateeeuuuh@gmail.com>
2024-12-05 17:15:58 +0100

    Interactibles
    Lever
    Receptor Light / Dark
    Door
    Door Animation

---
+++ Assets/Script/Interactible/LU_Lever.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> doorList;
+    [SerializeField] private bool isActivated;
+
+    private void Update()
+    {
+        if (isActivated)
+        {
+            foreach (GameObject door in doorList)
+            {
+                door.GetComponentInChildren<DoorOpening>().OpenDoor();
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!isActivated)
+        {
+            //if(other.CompareTag("Player") && interact)
+            //{
+                foreach (GameObject door in doorList)
+                {
+                    door.GetComponentInChildren<DoorOpening>().OpenDoor();
+                    isActivated = true;
+                }
+            //}
+        }
+    }
+}
