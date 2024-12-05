9a0c99ae299c94e58bfe9687732ab9025e6a51c1
vmalvoisin <v.malvoisin@lahorde.school>
2024-12-05 10:18:23 +0100

    Creation Folder unity

---
+++ Assets/Script/Sert_a_rien.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Sert_a_rien : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
