ff502eebd19f2165ac4fe6868bb4f92bb1c63c66
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-12-11 14:46:35 +0100

    audiomanager sliders

--- Assets/Script/Raphael/LU_Audio.cs
+++ Assets/Script/Raphael/LU_Audio.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LU_Audio : MonoBehaviour
 {
+    
     [Header("The Key of a sound follow the syntax : \n\nnameofthelist_indexofthesound")]
 
     [Header("Corresponding channel will be automatically \nchosen when giving the key")]
@@ -32,77 +33,79 @@ public class LU_Audio : MonoBehaviour
     [Header("fx_index")]
     [SerializeField] List<AudioClip> _FXSounds = new();
 
-    static Dictionary<string, AudioSource> _audioChannels = new();
+    static Dictionary<SoundType, AudioSource> _audioChannels = new();
     static Dictionary<string, AudioClip> _sounds = new();
+
 
     private void Awake()
     {
+        DontDestroyOnLoad(this.gameObject);
+        _audioChannels.Clear();
+        _sounds.Clear();
         for (int i = 0; i < _musics.Count; i++)
         {
-            string nameOfIndex = $"musics_{i}";
+            string nameOfIndex = $"{SoundType.musics}_{i}";
             _sounds.Add(nameOfIndex, _musics[i]);
         }
         for (int i = 0; i < _charactersSounds.Count; i++)
         {
-            string nameOfIndex = $"characters_{i}";
+            string nameOfIndex = $"{SoundType.characters}_{i}";
             _sounds.Add(nameOfIndex, _charactersSounds[i]);
         }
         for (int i = 0; i < _noctisSounds.Count; i++)
         {
-            string nameOfIndex = $"noctis_{i}";
+            string nameOfIndex = $"{SoundType.noctis}_{i}";
             _sounds.Add(nameOfIndex, _noctisSounds[i]);
         }
         for (int i = 0; i < _lumisSounds.Count; i++)
         {
-            string nameOfIndex = $"lumis_{i}";
+            string nameOfIndex = $"{SoundType.lumis}_{i}";
             _sounds.Add(nameOfIndex, _lumisSounds[i]);
         }
         for (int i = 0; i < _EnvironmentalsSounds.Count; i++)
         {
-            string nameOfIndex = $"environment_{i}";
+            string nameOfIndex = $"{SoundType.environment}_{i}";
             _sounds.Add(nameOfIndex, _EnvironmentalsSounds[i]);
         }
         for (int i = 0; i < _FXSounds.Count; i++)
         {
-            string nameOfIndex = $"fx_{i}";
+            string nameOfIndex = $"{SoundType.fx}_{i}";
             _sounds.Add(nameOfIndex, _FXSounds[i]);
         }
-        
-    }
+        _audioChannels.Add(SoundType.musics, transform.GetChild(0).GetComponent<AudioSource>());
+        _audioChannels.Add(SoundType.characters, transform.GetChild(1).GetComponent<AudioSource>());
+        _audioChannels.Add(SoundType.environment, transform.GetChild(2).GetComponent<AudioSource>());
+        _audioChannels.Add(SoundType.fx, transform.GetChild(3).GetComponent<AudioSource>());
 
-    private void Start()
-    {
-        _audioChannels.Add("musics",transform.GetChild(0).GetComponent<AudioSource>());
-        _audioChannels.Add("characters", transform.GetChild(1).GetComponent<AudioSource>());
-        _audioChannels.Add("environment", transform.GetChild(2).GetComponent<AudioSource>());
-        _audioChannels.Add("fx", transform.GetChild(3).GetComponent<AudioSource>());
     }
-    public static void PlaySound(string key)
+    public static void PlaySound(SoundType soundCategory, int index)
     {
         AudioClip audioToPlay;
+        string key = $"{soundCategory}_{index}";
         if (_sounds.TryGetValue(key, out audioToPlay))
         {
             string channelKey = key;
-            channelKey =channelKey.Remove( channelKey.IndexOf('_'));
+            channelKey = channelKey.Remove(channelKey.IndexOf('_'));
 
-            if (channelKey.Contains("noctis") || channelKey.Contains("lumis"))
+            if (channelKey.Contains(SoundType.noctis.ToString()) || channelKey.Contains(SoundType.noctis.ToString()))
             {
                 channelKey = channelKey.Replace(channelKey, "characters");
             }
-
-            if ( _audioChannels.TryGetValue(channelKey, out AudioSource channelToPlay))
-            {
-                channelToPlay.PlayOneShot( audioToPlay );
-            }
-            else
-            {
-                Debug.LogError($"channel non-existent : \n{channelKey}");
-            }
+            _audioChannels[soundCategory].PlayOneShot(audioToPlay);
         }
-        else 
+        else
         {
             Debug.LogError($"Key incorrect or audio non-existent : \n{key}");
         }
     }
+}
 
+public enum SoundType
+{
+    musics,
+    characters,
+    noctis,
+    lumis,
+    environment,
+    fx
 }
