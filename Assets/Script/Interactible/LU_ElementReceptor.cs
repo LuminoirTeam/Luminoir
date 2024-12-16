fe8b548d3c8aeb717400a912e0abf3691d74205d
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-12-16 17:19:13 +0100

    receptor done

--- Assets/Script/Interactible/LU_ElementReceptor.cs
+++ Assets/Script/Interactible/LU_ElementReceptor.cs
@@ -14,7 +14,7 @@ public class LU_ElementReceptor : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (!isActivated)
         {
