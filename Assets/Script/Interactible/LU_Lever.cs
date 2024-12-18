acccc541744f4d776a22f3e9f241c53bff44d8b9
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-12-18 10:25:24 +0100

    bug fix
    UI beginning and checkpoint colliders redo correctly

--- Assets/Script/Interactible/LU_Lever.cs
+++ Assets/Script/Interactible/LU_Lever.cs
@@ -5,19 +5,10 @@ public class Lever : MonoBehaviour
 {
     [SerializeField] private List<GameObject> doorList;
     public Animator leverAnimator;
-    [SerializeField] bool testLever=false;
 
     private void Start()
     {
         leverAnimator = GetComponentInChildren<Animator>();
-    }
-    private void Update()
-    {
-        if (testLever)
-        {
-            OpenDoor();
-            this.enabled = false;
-        }
     }
     public void OpenDoor()
     {
