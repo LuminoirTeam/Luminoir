3ef75abe196192d8203172df1b485c611b51e142
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-12-18 10:51:00 +0100

    end of level made modular for scene to load

--- Assets/Script/Raphael/LU_NextLevel.cs
+++ Assets/Script/Raphael/LU_NextLevel.cs
@@ -3,8 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class LU_NextLevel : MonoBehaviour
 {
+    //Used to load next scene
+    [SerializeField] int nextSceneBuildIndex;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(nextSceneBuildIndex);
     }
 }
